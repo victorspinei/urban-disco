b50071c3c471daa869793baf46d767986821769e
victorspinei <topgspineivictor@gmail.com>
2024-09-11 23:00:51 +0300

    extracted tracklist of the given album

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/victorspinei/urban-disco/internal/api"
+)
 
 func main() {
 	fmt.Println("Hello, world!")
+	api.GetSongsFromAlbum("seventh_son_of_a_seventh_son")
 }
